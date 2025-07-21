f6268f55c8686bca84ff66d46aed5e7830978c2d
sahithdaddla <daddlasahith>
2025-07-21 17:54:00 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -36,11 +36,11 @@ pool.connect((err, client, release) => {
 app.use(cors({
     origin: (origin, callback) => {
         const allowedOrigins = [
-            'http://44.223.23.145:5500',
+            'http://16.170.246.73:5500',
             'http://127.0.0.1:5500',
-            'http://44.223.23.145:3426',
-            'http://44.223.23.145:8049',
-            'http://44.223.23.145:8050',
+            'http://16.170.246.73:3426',
+            'http://16.170.246.73:8049',
+            'http://16.170.246.73:8050',
         ];
 
         console.log('CORS request from origin:', origin);
